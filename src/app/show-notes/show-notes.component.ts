cd83a0c77a6ae9b055c78e58e64e36c2119d8054
Khachaturr <grantovich91@gmail.com>
2021-07-19 02:54:18 +0400

    added notes show component

--- src/app/show-notes/show-notes.component.ts
+++ src/app/show-notes/show-notes.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Note, NoteService } from '../note.service';
 
 @Component({
   selector: 'app-show-notes',
   templateUrl: './show-notes.component.html',
   styleUrls: ['./show-notes.component.css']
 })
-export class ShowNotesComponent implements OnInit {
+export class ShowNotesComponent implements OnInit, OnDestroy  {
 
-  constructor() { }
+  dataNote: Note[];
+
+  formGroupforShowNote: FormGroup;
+
+  isshow: number;
+
+  
+
+  constructor(private formBilder: FormBuilder, private noteService: NoteService) {
+    this.formGroupforShowNote = this.formBilder.group({
+      titleShowNotes: ['', [Validators.required, Validators.maxLength(255)]],
+      descriptionShowNotes: ['', Validators.required]
+    })
+
+    this.noteService.send.subscribe(data => this.dataNote= [...data])
+   }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+     this.noteService.send.unsubscribe
+  }
+
+  openEditsection(){
+
+  };
+
+  deleteNote() {
+
+  };
+
+  remuveNotes() {
+
+  }
+
 
 }
